e3debea6b4dbedd71bc83cac602d881cc35e927e
mixteko <mixteko@gmail.com>
2026-06-16 18:09:48 -0600

    Corrige credenciales Supabase

--- config.js
+++ config.js
@@ -4,5 +4,5 @@
 
 const SUPABASE_CONFIG = {
   url: "https://szutmyfujgbfruurgjcl.supabase.co",
-  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6dXRteWZ1amdiZnJ1dXJnamNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk2NTI1NjIsImV4cCI6MjA2NTIyODU2Mn0.loE4ZbS-fhOsxIYQTF2wR_O01sx0KQzphW0ud-MHnKQ",
+  anonKey: "sb_publishable_jrQTCvdmUWP7OySQQ66Dxw_wprhhnWx",
 };
